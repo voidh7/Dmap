6ec3f12e0956c1bea1611f2dbda259757255e304
Void <vdev30301@gmail.com>
2025-08-22 18:26:24 -0300

    Update Dmap.py

--- Dmap.py
+++ Dmap.py
@@ -1,36 +1,25 @@
 import requests
 import os
-import config
-vertion = "1.0.0 beta"
-                                                  
-wordlist = [
-"/admin", "/xp-admin", "/administrator", "/cpainel", "/maneger/html",
-"/login", "/signin", "/reset-password", "/upload", "/files", "/media",
-"/images", "/config", "/bug", "/debug", "/phpinfo.php", "/db",                                      "/database", "/sql"
-]
+import config                                                         import wl
 
+                                                                      
 if config.initClear == True:
     os.system("clear")
 
-if config.banner == True:
-    os.system("figlet Dmap")
+if config.banner == True:                                                 os.system("figlet Dmap")
     print("by voidh7")
-    print(f"vertion:{vertion}")
-    print("-----------------")
+    print(f"vertion:{config.vertion}")                                    print("-----------------")
 else:
     print("DMAP by voidh7")
     print(f"vertion {config.vertion}")
-    print("-----------------")
+    print("-----------------")                                        
 
+url = input("what is the url:")                                       
 
-
-url = input("what is the url:")
-
-
-for i in range(len(wordlist)):
+for i in range(len(wl.wordlist)):
    i+=1
 
-   getUrl = f"{url}{wordlist[i]}"
+   getUrl = f"{url}{wl.wordlist[i]}"
    response = requests.get(getUrl)
 
    if response.status_code == 200:
@@ -40,5 +29,4 @@ for i in range(len(wordlist)):
         if config.verbose == True:
             print(f"{getUrl} {response.status_code}")
         else:
-            print("[;-;] errror ")
- 
+            print("[;-;] error ") 
