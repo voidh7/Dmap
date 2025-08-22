0df45583de4ee057bd64007c7df9c02e28639c4a
Void <vdev30301@gmail.com>
2025-08-22 17:42:26 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-vertion = "1.0.0 beta"
+vertion = "1.0.1 beta"
 verbose = False # set for true to  avoid verbose mode
 initClear = False # set for true to clear screen
- 
+ banner = True # Change to false in case you want to download figlet
