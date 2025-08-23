b2bad350474bfd748c8a329ab84587f3cd389617
Void <vdev30301@gmail.com>
2025-08-22 22:03:48 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-vertion = "1.0.1 beta"
-verbose = False # set for true to  avoid verbose mode
+vertion = "1.1.0 beta"
+verbose = True # set for true to  avoid verbose mode
 initClear = False # set for true to clear screen
- banner = True # Change to false in case you want to download figlet
+banner = True # Change to false in case you want to download figlet
+outputFile = True # If true, all the program’s positive output will be saved in a .txt file
+timeout = 3# limit time for wensite response Change to 3 for a faster scan (you might miss some endpoints)
