ada43aa2cace9ae3b4f2f00c1ec5c664d92b0253
Void <vdev30301@gmail.com>
2025-08-22 22:05:06 -0300

    Update Dmap.py
    vertion 1.1.0 new output files and timeout

--- Dmap.py
+++ Dmap.py
@@ -1,32 +1,49 @@
 import requests
 import os
-import config                                                         import wl
+import config
+import wl
 
-                                                                      
+
 if config.initClear == True:
     os.system("clear")
 
-if config.banner == True:                                                 os.system("figlet Dmap")
+if config.banner == True:
+    os.system("figlet Dmap")
     print("by voidh7")
-    print(f"vertion:{config.vertion}")                                    print("-----------------")
+    print(f"vertion:{config.vertion}")
+    print("-----------------")
 else:
     print("DMAP by voidh7")
     print(f"vertion {config.vertion}")
-    print("-----------------")                                        
+    print("-----------------")
 
-url = input("what is the url:")                                       
+url = input("what is the url:")
+
+
+if config.outputFile == True:
+    outputFilename = input("output fileName:")
+    open(outputFilename,"w").close()
+    with open(outputFilename,"a") as file:
+        file.write(f" \n BY DMAP {config.vertion} \n ")
+
+
 
 for i in range(len(wl.wordlist)):
-   i+=1
+    getUrl = f"{url}{wl.wordlist[i]}"
+    try:
+        response = requests.get(getUrl, timeout=config.timeout)
 
-   getUrl = f"{url}{wl.wordlist[i]}"
-   response = requests.get(getUrl)
-
-   if response.status_code == 200:
-      print(f"{getUrl} [open status code 200]")
-
-   else:
-        if config.verbose == True:
-            print(f"{getUrl} {response.status_code}")
+        if response.status_code == 200:
+            print(f"{getUrl} [open status code 200]")
+            with open(outputFilename,"a") as file:
+                file.write(f"{getUrl} [open status code 200]\n")
         else:
-            print("[;-;] error ") 
+            if config.verbose == True:
+                print(f"{getUrl} {response.status_code}")
+            else:
+                print("[;-;] error")
+
+    except requests.exceptions.RequestException:
+        print(f"{getUrl} → erro ou timeout")
+
+
